333595e22afb54b70391a2fd57d631cfce408c52
JoshuaSchreiber <joshua.schreiber.bg@bs-gn.de>
2024-08-05 21:36:05 +0200

    d

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -19,8 +19,89 @@ public class Person {
         vorname = parts[2];
         String[] dateParts = parts[3].split(".");
         gebDate = new Date(Integer.parseInt(dateParts[2]), Integer.parseInt(dateParts[1]), Integer.parseInt(dateParts[0]));
+    }
 
-        System.out.println(personenID + nachname + vorname);
-        System.out.println(gebDate);
+    public String toString(){
+        return personenID + " , " + nachname + " , " + vorname + " , " + gebDate.toString() + " , " + plz + " , " + ort + " , " + strasse + " , " + risiko + " , " + einverstaendnis + " , " + anzahlImpfungen;
+    }
+
+    public int getPersonenID() {
+        return personenID;
+    }
+
+    public void setPersonenID(int personenID) {
+        this.personenID = personenID;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public Date getGebDate() {
+        return gebDate;
+    }
+
+    public void setGebDate(Date gebDate) {
+        this.gebDate = gebDate;
+    }
+
+    public String getPlz() {
+        return plz;
+    }
+
+    public void setPlz(String plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public boolean isRisiko() {
+        return risiko;
+    }
+
+    public void setRisiko(boolean risiko) {
+        this.risiko = risiko;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public boolean isEinverstaendnis() {
+        return einverstaendnis;
+    }
+
+    public void setEinverstaendnis(boolean einverstaendnis) {
+        this.einverstaendnis = einverstaendnis;
+    }
+
+    public int getAnzahlImpfungen() {
+        return anzahlImpfungen;
+    }
+
+    public void setAnzahlImpfungen(int anzahlImpfungen) {
+        this.anzahlImpfungen = anzahlImpfungen;
     }
 }
---
+++ src/main/java/Starter.java
@@ -0,0 +1,6 @@
+public class Starter {
+    public static void Number1s1(){
+        Person person = new Person("77665;Müller;Klaus;31.12.1980;35334;Hausen;Teichweg 8;1 ");
+        System.out.println(person.toString());
+    }
+}
