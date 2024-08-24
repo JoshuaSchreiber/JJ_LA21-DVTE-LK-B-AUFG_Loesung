86a4f4c03bb22815e254affb4e88a17b7654c271
Jonas Krisch <136814252+Jonas-Krisch@users.noreply.github.com>
2024-08-24 11:38:35 +0200

    einladen() von Impfzentrumverwaltung noch korigieren, segmentieren und kommentieren

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        Starter.Number1s3();
+        Starter.Number1s1();
     }
 }
