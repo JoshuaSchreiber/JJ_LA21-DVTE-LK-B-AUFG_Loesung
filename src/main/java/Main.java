c4521b2f087b6d901d63ae3e7916d5a69e9308f3
JoshuaSchreiber <joshua.schreiber.bg@bs-gn.de>
2024-09-04 20:19:10 +0200

    gitignore .ides/workspace.xml Aufgabe 2.1.2 wip

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,6 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        Starter.number1s3();
-        Starter.number1s4();
+        Starter.number1s1();
     }
 }
